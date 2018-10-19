03401aeac0c9cf57e6a15a9ebef761a32ae77c17
Martin Simonovski <martin.simonovski@gmail.com>
2018-10-19 17:24:23 +0200

    work on the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as authActions from '../../store/actions';
@@ -17,24 +17,43 @@ class Header extends Component {
         this.props.dispatch(authActions.logout());
     }
 
+    renderAuthenticatedButtons() {
+        if (this.props.auth.authenticated) {
+            return (
+                <Fragment>
+                    <a className="button"><strong>Persons</strong></a>
+                    <a className="button"><strong>Create Group</strong></a>
+                </Fragment>
+            )
+        }
+        return '';
+    }
+
     render() {
         const authButton = this.props.auth.authenticated ?
-            (<Link to="/" onClick={this.handleLogout}>Logout</Link>) :
-            (<Link to="/login" className="navbar-item">Login</Link>);
+            (<Link to="/" className="button is-light" onClick={this.handleLogout}>Logout</Link>) :
+            (<Link to="/login" className="button is-light">Login</Link>);
 
         return (
             <nav className="navbar has-shadow is-spaced">
                 <section className="container">
                     <div className="navbar-brand">
-                        <Link to="/" className="navbar-item">CAKE</Link>
+                        <Link to="/" className="navbar-item">
+                            <img src="/cake.png" /> <strong>CAKE</strong>
+                        </Link>
                     </div>
                     <div className="navbar-menu">
                         <div className="navbar-end">
-                            {authButton}
+
+                            <div className="buttons">
+                                <a className="button"><strong>Payments</strong></a>
+                                {this.renderAuthenticatedButtons()}
+                                {authButton}
+                            </div>
                         </div>
                     </div>
                 </section>
-            </nav>
+            </nav >
         );
     };
 }
